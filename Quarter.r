16b647ba76345680f70e47d17a710681bb10349a
Sam Jake <samjulian88@gmail.com>
2016-04-08 02:48:26 +0530

    Quarter - start dates & end dates calculated -v3

--- Quarter.r
+++ Quarter.r
@@ -4,10 +4,12 @@ library(ggplot2)
 #install.packages("nlme")
 library(nlme)
 
+#install.packages("lubridate")
+library(lubridate)
 
 classes <- c("character","numeric","numeric","numeric","numeric","integer","numeric")
 names(classes) <- c("Trade_dt","Open","High","Low","Close","Volume","Adj Close")
-df <- read.csv(file = "J:/R/Test/Data/Stock_files_/stock_SWELECT.csv",colClasses = classes)
+df <- read.csv(file = "J:/R/Test/test/Data/Stock_files_/stock_SWELECT.csv",colClasses = classes)
 df$Date <- as.Date(df$Date,format="%m/%d/%Y")
 
 #curr_dt <- Sys.Date()
@@ -19,11 +21,17 @@ q_mid <- c("-02-15","-05-15","-08-15","-11-15")
 q_mid <- paste(y,q_mid,sep="")
 q_mid <- as.Date(q_mid,format = "%Y-%m-%d")
 
-q_start <- c("-01-01","-04-01","-07-01","-10-01")
-q_start <- paste(y,q_start,sep="")
-q_start <- as.Date(q_start,format = "%Y-%m-%d")
+for(i in 1:length(q_mid))
+{
+	if(df_dt >= q_mid[i])
+	{
+		temp_dt <- q_mid[i]
+	}
+}
 
-q_end <- c("-02-15","-05-15","-08-15","-11-15")
-q_end <- paste(y,q_end,sep="")
-q_end <- as.Date(q_end,format = "%Y-%m-%d")
+frst_q_strt_dt <- round_date(temp_dt,"quarter")
+q_start <- c(frst_q_strt_dt,frst_q_strt_dt -months(3),frst_q_strt_dt -months(6),frst_q_strt_dt -months(9))
 
+
+q_end <- q_start + months(3) - days(1)
+q_end[1] <- df_dt
