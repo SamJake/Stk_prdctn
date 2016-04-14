3237b841bff977fa5b419e068207ddf2311482a4
Sam Jacob <Sam.Jacob@synechron.com>
2016-04-14 20:24:40 +0530

    Quarter - Plot comparing last 4 quarters performance across same timeline -v7

--- Quarter.r
+++ Quarter.r
@@ -53,20 +53,53 @@ m4 <- matrix(4,length(q4_df$Date))
 #g3 <- ggplot(q3_df,aes(x=Date,y=Close)) + geom_line(size=1)
 #g4 <- ggplot(q4_df,aes(x=Date,y=Close)) + geom_line(size=1)
 
+d1 <- length(m1):1
+d2 <- length(m2):1
+d3 <- length(m3):1
+d4 <- length(m4):1
 
-q1 <- cbind(q1_df,m1)
-q2 <- cbind(q2_df,m2)
-q3 <- cbind(q3_df,m3)
-q4 <- cbind(q4_df,m4)
+q1 <- cbind(d1,q1_df,m1)
+q2 <- cbind(d2,q2_df,m2)
+q3 <- cbind(d3,q3_df,m3)
+q4 <- cbind(d4,q4_df,m4)
 
 
-colnames(q1)[8] <- "m"
-colnames(q2)[8] <- "m"
-colnames(q3)[8] <- "m"
-colnames(q4)[8] <- "m"
+colnames(q1)[9] <- "m"
+colnames(q2)[9] <- "m"
+colnames(q3)[9] <- "m"
+colnames(q4)[9] <- "m"
 
+colnames(q1)[1] <- "d"
+colnames(q2)[1] <- "d"
+colnames(q3)[1] <- "d"
+colnames(q4)[1] <- "d"
 
 Q <- rbind(q1,q2,q3,q4)
 
-g <- ggplot(Q,aes(x=Date,y=Close,color=factor(m))) + geom_line(size=1.2)
-g <- g + ylab("Stock price in INR") + xlab("Last 4 quarters") + ggtitle("NSE: SWELECTES")
+#g <- ggplot(Q,aes(x=Date,y=Close,color=factor(m))) + geom_line(size=1.2)
+#g <- g + ylab("Stock price in INR") + xlab("Last 4 quarters") + ggtitle("NSE: SWELECTES")
+
+g1 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_smooth(size=1.2)
+g1 <- g1 + scale_color_manual(values=c("red","pink3","orange","yellow"))
+
+g2 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_line(size=1.2)
+g2 <- g2 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"))
+g2 <- g2 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
+g2
+
+g3 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_smooth(size=1.2)
+g3 <- g3 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"))
+g3 <- g3 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
+g3 <- g3 + scale_colour_discrete(name="Quarters:\nRed -> Current;\nBlue -> Oldest")
+g3
+
+g4 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_line(size=1.2)
+g4 <- g4 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"))
+g4 <- g4 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
+g4 <- g4 + facet_wrap(~m,ncol=1)
+g4
+
+g5 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_smooth(size=1.2)
+g5 <- g5 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"),breaks=c("1","2","3","4"),labels=c("Q1","Q2","Q3","Q4"),name="Quarters:\nQ1 -> Current;\nQ4 -> Oldest")
+g5 <- g5 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
+g5
