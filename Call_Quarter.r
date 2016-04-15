3eba60b2510eb336acefacf14497b7b54785d88f
Sam Jacob <Sam.Jacob@synechron.com>
2016-04-15 18:26:15 +0530

    Quarter | Functionalized for running diff tickers -v7

---
+++ Call_Quarter.r
@@ -0,0 +1,25 @@
+Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/BRK-A.csv")
+g1 <- stock_q_graph(Q)
+g2 <- stock_q_graph_smooth(Q)
+g1
+
+Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/BRK-B.csv")
+g1 <- stock_q_graph(Q)
+g2 <- stock_q_graph_smooth(Q)
+g1
+
+Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/YHOO.csv")
+g1 <- stock_q_graph(Q)
+g2 <- stock_q_graph_smooth(Q)
+g1
+
+Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/GOOG.csv")
+g1 <- stock_q_graph(Q)
+g2 <- stock_q_graph_smooth(Q)
+g1
+
+Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/MSFT.csv")
+g1 <- stock_q_graph(Q)
+g2 <- stock_q_graph_smooth(Q)
+g1
+
--- Quarter.r
+++ Quarter.r
@@ -7,10 +7,13 @@ library(nlme)
 #install.packages("lubridate")
 library(lubridate)
 
-classes <- c("character","numeric","numeric","numeric","numeric","integer","numeric")
+
+get_q_stock <- function(file_path)
+{
+classes <- c("character","numeric","numeric","numeric","numeric","numeric","numeric")
 names(classes) <- c("Trade_dt","Open","High","Low","Close","Volume","Adj Close")
-df <- read.csv(file = "J:/R/Test/test/Data/Stock_files_/stock_SWELECT.csv",colClasses = classes)
-df$Date <- as.Date(df$Date,format="%m/%d/%Y")
+df <- read.csv(file = file_path,colClasses = classes)
+df$Date <- as.Date(df$Date,format="%Y-%m-%d")
 
 #curr_dt <- Sys.Date()
 df_dt <- df[[1,1]]
@@ -48,11 +51,6 @@ m2 <- matrix(2,length(q2_df$Date))
 m3 <- matrix(3,length(q3_df$Date))
 m4 <- matrix(4,length(q4_df$Date))
 
-#g1 <- ggplot(q1_df,aes(x=Date,y=Close)) + geom_line(size=1)
-#g2 <- ggplot(q2_df,aes(x=Date,y=Close)) + geom_line(size=1)
-#g3 <- ggplot(q3_df,aes(x=Date,y=Close)) + geom_line(size=1)
-#g4 <- ggplot(q4_df,aes(x=Date,y=Close)) + geom_line(size=1)
-
 d1 <- length(m1):1
 d2 <- length(m2):1
 d3 <- length(m3):1
@@ -75,31 +73,24 @@ colnames(q3)[1] <- "d"
 colnames(q4)[1] <- "d"
 
 Q <- rbind(q1,q2,q3,q4)
+return(Q)
+}
 
-#g <- ggplot(Q,aes(x=Date,y=Close,color=factor(m))) + geom_line(size=1.2)
-#g <- g + ylab("Stock price in INR") + xlab("Last 4 quarters") + ggtitle("NSE: SWELECTES")
+stock_q_graph <- function(Q)
+{
+g1 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_line(size=1.2)
+g1 <- g1 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"),breaks=c("1","2","3","4"),labels=c("Q1(Current)","Q2","Q3","Q4(Oldest)"),name="Quarters:")
+g1 <- g1 + xlab("Days in a Quarter") + ylab("Stock price in USD") + ggtitle("NYSE: BRK-A")
+return(g1)
+}
 
-g1 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_smooth(size=1.2)
-g1 <- g1 + scale_color_manual(values=c("red","pink3","orange","yellow"))
+stock_q_graph_smooth <- function(Q)
+{
+g2 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_smooth(size=1.2)
+g2 <- g2 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"),breaks=c("1","2","3","4"),labels=c("Q1(Current)","Q2","Q3","Q4(Oldest)"),name="Quarters:")
+g2 <- g2 + xlab("Days in a Quarter") + ylab("Stock price in USD") + ggtitle("NYSE: BRK-A")
+return(g2)
+}
 
-g2 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_line(size=1.2)
-g2 <- g2 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"))
-g2 <- g2 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
-g2
 
-g3 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_smooth(size=1.2)
-g3 <- g3 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"))
-g3 <- g3 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
-g3 <- g3 + scale_colour_discrete(name="Quarters:\nRed -> Current;\nBlue -> Oldest")
-g3
 
-g4 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_line(size=1.2)
-g4 <- g4 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"))
-g4 <- g4 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
-g4 <- g4 + facet_wrap(~m,ncol=1)
-g4
-
-g5 <- ggplot(Q,aes(x=d,y=Adj.Close,color=factor(m))) + geom_smooth(size=1.2)
-g5 <- g5 + scale_color_manual(values=c("red2","yellow3","yellowgreen","lightblue2"),breaks=c("1","2","3","4"),labels=c("Q1","Q2","Q3","Q4"),name="Quarters:\nQ1 -> Current;\nQ4 -> Oldest")
-g5 <- g5 + xlab("Days in a Quarter") + ylab("Stock price in INR") + ggtitle("NSE: SWELECTES")
-g5
