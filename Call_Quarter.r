2120bde0618f6181592cd2bede61dad3689efe6d
Sam Jacob <Sam.Jacob@synechron.com>
2016-04-18 19:42:14 +0530

    Quarterly plots | automatically saving plots -v9

--- Call_Quarter.r
+++ Call_Quarter.r
@@ -1,25 +1,59 @@
+setwd("E:/R/Repository/Stk_prdctn/Sample plots/4quarters/NYSE")
+dev.off()
+
+
 Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/BRK-A.csv")
-g1 <- stock_q_graph(Q,"NYSE","BRK-A")
-g2 <- stock_q_graph_smooth(Q,"NYSE","BRK-A")
+sym <- "BRK-A"
+g1 <- stock_q_graph(Q,"NYSE",sym)
 g1
+dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
+dev.off()
+g2 <- stock_q_graph_smooth(Q,"NYSE",sym)
+g2
+dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
+dev.off()
+
 
 Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/BRK-B.csv")
 g1 <- stock_q_graph(Q,"NYSE","BRK-B")
-g2 <- stock_q_graph_smooth(Q,"NYSE","BRK-B")
 g1
+dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
+dev.off()
+g2 <- stock_q_graph_smooth(Q,"NYSE","BRK-B")
+g2
+dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
+dev.off()
+
 
 Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/YHOO.csv")
 g1 <- stock_q_graph(Q,"NYSE","YHOO")
-g2 <- stock_q_graph_smooth(Q,"NYSE","YHOO")
 g1
+dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
+dev.off()
+g2 <- stock_q_graph_smooth(Q,"NYSE","YHOO")
+g2
+dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
+dev.off()
+
 
 Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/GOOG.csv")
 g1 <- stock_q_graph(Q,"NYSE","GOOG")
-g2 <- stock_q_graph_smooth(Q,"NYSE","GOOG")
 g1
+dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
+dev.off()
+g2 <- stock_q_graph_smooth(Q,"NYSE","GOOG")
+g2
+dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
+dev.off()
+
 
 Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/MSFT.csv")
 g1 <- stock_q_graph(Q,"NYSE","MSFT")
+g1
+dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
+dev.off()
 g2 <- stock_q_graph_smooth(Q,"NYSE","MSFT")
 g2
+dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
+dev.off()
 
