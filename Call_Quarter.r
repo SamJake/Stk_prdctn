131dcb9440a2b7e98b0b12f870ce341de1552086
Sam Jacob <Sam.Jacob@synechron.com>
2016-04-18 20:07:05 +0530

    Quarterly plots | Process a vector of plots -v10

--- Call_Quarter.r
+++ Call_Quarter.r
@@ -1,59 +1,24 @@
 setwd("E:/R/Repository/Stk_prdctn/Sample plots/4quarters/NYSE")
-dev.off()
+#dev.off()
+graphics.off()
+tickers <- c("BRK-A","BRK-B","YHOO","GOOG","MSFT")
+
+for (sym in tickers)
+{
+	Q <- get_q_stock(paste("E:/R/Repository/Stk_prdctn/Test data/NYSE/",sym,".csv",sep=""))
+	g1 <- stock_q_graph(Q,"NYSE",sym)
+	#dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
+	#dev.off()
+	ggsave(filename=paste("4q - ",sym,".jpeg",sep=""))
+	graphics.off()
+	g2 <- stock_q_graph_smooth(Q,"NYSE",sym)
+	g2
+	#dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
+	#dev.off()
+	ggsave(filename=paste("4q - ",sym,"_smooth.jpeg",sep=""))
+	graphics.off()
+}
 
 
-Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/BRK-A.csv")
-sym <- "BRK-A"
-g1 <- stock_q_graph(Q,"NYSE",sym)
-g1
-dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
-dev.off()
-g2 <- stock_q_graph_smooth(Q,"NYSE",sym)
-g2
-dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
-dev.off()
 
-
-Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/BRK-B.csv")
-g1 <- stock_q_graph(Q,"NYSE","BRK-B")
-g1
-dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
-dev.off()
-g2 <- stock_q_graph_smooth(Q,"NYSE","BRK-B")
-g2
-dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
-dev.off()
-
-
-Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/YHOO.csv")
-g1 <- stock_q_graph(Q,"NYSE","YHOO")
-g1
-dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
-dev.off()
-g2 <- stock_q_graph_smooth(Q,"NYSE","YHOO")
-g2
-dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
-dev.off()
-
-
-Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/GOOG.csv")
-g1 <- stock_q_graph(Q,"NYSE","GOOG")
-g1
-dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
-dev.off()
-g2 <- stock_q_graph_smooth(Q,"NYSE","GOOG")
-g2
-dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
-dev.off()
-
-
-Q <- get_q_stock("E:/R/Repository/Stk_prdctn/Test data/NYSE/MSFT.csv")
-g1 <- stock_q_graph(Q,"NYSE","MSFT")
-g1
-dev.copy(jpeg,paste("4q - ",sym,".jpeg",sep=""))
-dev.off()
-g2 <- stock_q_graph_smooth(Q,"NYSE","MSFT")
-g2
-dev.copy(jpeg,paste("4q_smooth - ",sym,".jpeg",sep=""))
-dev.off()
 
