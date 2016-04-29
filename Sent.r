9f3a5d04f0a6df57c34f24d036e472ca8bd8ff90
Sam Jacob <Sam.Jacob@synechron.com>
2016-04-29 15:13:17 +0530

    Text mining | Functionized - v7

--- Sent.r
+++ Sent.r
@@ -12,62 +12,45 @@ library("RCurl")
 library("openNLP")
 library("qdap")
 
-stock <- "NASDAQ:DELL"
-company <- "Dell"
+stock <- "GOOGL"
+company <- "Google"
+type <- 1
 
-#assign(paste("F_",stock,sep=""),WebCorpus(GoogleFinanceSource(stock)))
-
-
-F_DELL <- WebCorpus(GoogleFinanceSource("GOOGL"))
-
-#GOOGL_fin <- corpus.update(GOOGL_fin)
-
-#assign(paste("N_",company,sep=""),WebCorpus(GoogleNewsSource(company)))
-
-N_Dell <- WebCorpus(GoogleNewsSource("Google"))
-
-#GOOGL_news <- corpus.update(GOOGLnews)
-
-
-
-sF_DELL <- sent_detect(F_DELL)
-sN_Dell <- sent_detect(N_Dell)
-
-fil_sF_DELL <- sF_DELL[grepl(stock,sF_DELL,ignore.case = FALSE)]
-fil_sN_Dell <- sN_Dell[grepl(company,sN_Dell,ignore.case = FALSE)]
-
-
-# hF_DELL <- sapply(F_DELL,FUN = function(x){attr(x,"heading")})
-# hN_Dell <- sapply(N_Dell,FUN = function(x){attr(x,"heading")})
-
-
-# dF_DELL <- sapply(F_DELL, FUN = function(x){attr(x,"description")})
-# dN_Dell <- sapply(N_Dell, FUN = function(x){attr(x,"description")})
-
-#heading => id
-#description => id
-
-fil_dF_DELL <- dF_DELL[grepl(stock,dF_DELL,ignore.case = FALSE)]
-fil_dN_Dell <- dN_Dell[grepl(company,dN_Dell,ignore.case = FALSE)]
-
-meta(F_DELL[[1]])
-str(meta(F_DELL[[1]]))
-meta(F_DELL[[1]])$heading
-meta(F_DELL[[1]])$description
-
-
-
-heading <- vector()
-for (i in 1:length(F_DELL$content))
+text_mining <- function(stock,type)
 {
-	heading[i] <- meta(F_DELL[[i]])$heading
+
+if (type==1)
+{
+	corpus <- WebCorpus(GoogleFinanceSource(stock))
+	#GOOGL_fin <- corpus.update(GOOGL_fin)
+}
+else if(type==2)
+{
+	corpus <- WebCorpus(GoogleNewsSource(stock))
+	#GOOGL_news <- corpus.update(GOOGLnews)
+}
+
+	sentences <- sent_detect(corpus)
+	
+	headings <- vector()
+	for (i in 1:length(corpus$content))
+	{
+		headings[i] <- meta(corpus[[i]])$heading
+	}
+
+	descriptions <- vector()
+	for ( i in 1:length(corpus$content))
+	{
+		descriptions[i] <- meta(corpus[[i]])$description
+	} 
+
+	select_sentences <- sentences[grepl(stock,sentences,ignore.case = FALSE)]
+	select_headings <- headings[grepl(stock,headings,ignore.case = FALSE)]
+	select_descriptions <- descriptions[grepl(stock,descriptions,ignore.case = FALSE]
+
+
 }
 
 
-
-
-description <- vector()
-for ( i in 1:length(F_DELL$content))
-{
-    description[i] <- meta(F_DELL[[i]])$description
-} 
+text_mining(stock,1)
+text_mining(company,2)
